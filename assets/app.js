a103204e4b188affcc16a8525d498d19eb933114
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-02-07 08:59:11 +0200

    menu

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,10 @@ const summernote = require('summernote');
 $('#summernote').summernote();
 
 console.log('summernote');
+
+//hamburgerio click
+    const hamburger = document.querySelector('.mob-menu .hamburger');
+    const nav = document.querySelector('.mob-menu nav');
+        hamburger.addEventListener('click', () => {
+            nav.classList.toggle('visible');
+        });
